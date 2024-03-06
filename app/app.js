d4ee9f1f7c768e244926888e6e721356da017843
quentin doniczka <doniczka.quentin67@gmail.com>
2024-03-06 17:13:56 +0100

    test

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 const http = require('node:http');
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = 3001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('there is app.js\n');
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
